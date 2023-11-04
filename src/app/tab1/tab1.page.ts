efe63299c99b5ae030890d6cc1e8bffcc84901a6
jaime904 <jai.aravena@duocuc.cl>
2023-11-04 20:42:01 -0300

    Se agrega el conteo de viajes del usuario

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+   public contador: number = 0;
   constructor() {}
 
+  botonClick() {
+    this.contador++;
+    return this.contador;
+  }
 }
